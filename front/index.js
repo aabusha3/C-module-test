34a667da32633b53470627706e0a6c9dfd0cc22a
aabusha3 <aabusha3@uwo.ca>
2023-06-23 08:33:57 -0400

    finalize front skeleton

---
+++ front/index.js
@@ -0,0 +1,11 @@
+document.getElementById('gcc').addEventListener('click', ()=>{
+    location.href = "./gcc.html";
+});
+
+document.getElementById('node').addEventListener('click', ()=>{
+    location.href = "./node.html";
+});
+
+document.getElementById('wasm').addEventListener('click', ()=>{
+    location.href = "./wasm.html";
+});
