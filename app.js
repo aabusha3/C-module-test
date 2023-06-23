13b756fa5ea83af6f7d94e49eb9c808b6f3c47e8
aabusha3 <90729952+aabusha3@users.noreply.github.com>
2023-06-23 11:40:24 -0400

    add so router functionalities

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const NanoTimer = require('nanotimer');
+const ffi = require('ffi-napi');
 
 const app = express();
 const PORT = 7999;
@@ -7,15 +9,69 @@ const PORT = 7999;
 app.use(express.json());
 app.use(cors());
 
+const soRouter = express.Router();
+const nodeRouter = express.Router();
+const wasmRouter = express.Router();
+
+// soRouter.use(express.json());
+
 app.use(express.static('./front'));
-
-
-// app.use("/api/outline",require('./routes/Outline'));
+app.use('/api/so', soRouter)
+app.use('/api/node', nodeRouter)
+app.use('/api/wasm', wasmRouter)
 
 app.use((req,res,next)=>{
     console.log(`${req.method} request for ${req.url}`);
     next();
 });
+
+
+const soAddon = ffi.Library('./gcc/tt', {
+    "enter":["void", ["int"],],
+})
+soRouter.route('/:size/:repeat')
+.get((req,res)=>{
+    const size = req.params.size;
+    const repeat = req.params.repeat;
+    
+    let time = new NanoTimer().time((() => {
+        for(let i=0; i<repeat; i++) {
+            soAddon.enter(size);
+        }
+    }),'','u')
+
+    return res.send({"time":usToHMS(time),"format":"(hours:minutes:seconds.milli.micro)"});
+});
+
+nodeRouter.route('/:size/:repeat')
+.get((req,res)=>{
+    const size = req.params.size;
+    const repeat = req.params.repeat;
+    
+    let time = new NanoTimer().time((() => {
+        for(let i=0; i<repeat; i++) {
+            soAddon.enter(size);
+        }
+    }),'','u')
+    
+    return res.send({"time":usToHMS(time),"format":"(hours:minutes:seconds.milli.micro)"});
+});
+
+
+function usToHMS( us ) {
+    var ms = us /1000
+    var seconds = ms / 1000;
+    ms = ms % 1000
+    
+    var hours = parseInt( seconds / 3600 );
+    seconds = seconds % 3600;
+    
+    var minutes = parseInt( seconds / 60 ); 
+    seconds = seconds % 60;
+    
+    us = us % 1000
+    return ( hours+":"+minutes+":"+Math.trunc(seconds)+"."+Math.trunc(ms)+"."+Math.trunc(us));
+}
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`);
--- front/index.js
+++ front/index.js
@@ -1,11 +1,45 @@
-document.getElementById('gcc').addEventListener('click', ()=>{
-    location.href = "./gcc.html";
-});
+document.getElementById('gcc-search').addEventListener('click', async()=>{await checkInput('gcc')});
+document.getElementById('node-search').addEventListener('click', async()=>{await checkInput('node')});
+document.getElementById('wasm-search').addEventListener('click', async()=>{await checkInput('wasm')});
 
-document.getElementById('node').addEventListener('click', ()=>{
-    location.href = "./node.html";
-});
+ async function checkInput(type){
+    const size = document.getElementById('size').value;
+    const repeat = document.getElementById('repeat').value;
 
-document.getElementById('wasm').addEventListener('click', ()=>{
-    location.href = "./wasm.html";
-});
+    if(size ==='')
+        return alert('Please Make Sure To Enter In A Number In The Size Field');    
+        
+    if(repeat ==='')
+        return alert('Please Make Sure To Enter In A Number In The Repeat Field');
+
+    if(parseInt(size) <= 0||parseInt(repeat) <= 0)
+        return alert('Please Make Sure To Enter In A Positive Number');
+    
+    // console.log(type)
+    // console.log(size)
+    // console.log(repeat)
+
+    await makeCall(type, size, repeat);
+}
+
+async function makeCall(type, size, repeat){
+    switch(type){
+        case 'gcc':
+            fetch(`/api/so/${size}/${repeat}`).then(res=>res.json().then(data=>{
+                console.log(`${data.time}  ${data.format}`)
+            }));
+            break;
+        case 'node':
+            fetch(`/api/node/${size}/${repeat}`).then(res=>res.json().then(data=>{
+                
+            }));
+            break;
+        case 'wasm':
+            fetch(`/api/wasm/${size}/${repeat}`).then(res=>res.json().then(data=>{
+                
+            }));
+            break;    
+    }
+}
+
+
